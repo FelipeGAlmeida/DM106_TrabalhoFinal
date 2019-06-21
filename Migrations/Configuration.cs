e7de3daf62e7b5038271f96c3c0e9fe2dde64a09
felipegalmeida <felipegalmeida@outlook.com>
2019-06-21 17:04:48 -0300

    Partes de Produtos Adicionadas

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,58 @@
+namespace DM106_TF.Migrations
+{
+    using DM106_TF.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DM106_TF.Models.DM106_TFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DM106_TF.Models.DM106_TFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+
+            context.Products.AddOrUpdate(
+                p => p.Id,
+                new Product {
+                    Id = 1,
+                    nome = "produto 1",
+                    descricao = "descrição produto 1",
+                    cor = "Verde",
+                    modelo = "Modelo1",
+                    codigo = "COD1",
+                    preco = 10,
+                    peso = 1,
+                    altura = 1,
+                    largura = 1,
+                    comprimento = 1,
+                    diametro = 1,
+                    Url = "www.siecolasystems.com/produto1"
+                },
+                new Product {
+                    Id = 2,
+                    nome = "produto 2",
+                    codigo = "COD2",
+                    descricao = "descrição produto 2",
+                    cor = "Vermelho",
+                    modelo = "Modelo2",
+                    preco = 20,
+                    peso = 2,
+                    altura = 2,
+                    largura = 2,
+                    comprimento = 2,
+                    diametro = 2,
+                    Url = "www.siecolasystems.com/produto2"
+                }
+            );
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DM106_TF.Models {
+    public class Product {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        public string nome { get; set; }
+
+        public string descricao { get; set; }
+
+        public string cor { get; set; }
+
+        [Index(IsUnique = true)]
+        [Required(ErrorMessage = "O campo modelo é obrigatório")]
+        [StringLength(50, ErrorMessage = "O tamanho	máximo do modelo é 50 caracteres")]
+        public string modelo { get; set; }
+
+        [Index(IsUnique = true)]
+        [Required(ErrorMessage = "O campo codigo é obrigatório")]
+        [StringLength(8, ErrorMessage = "O tamanho	máximo do código é 8 caracteres")]
+        public string codigo { get; set; }
+
+        [Required(ErrorMessage = "O campo preço é obrigatório")]
+        [Range(1, 999, ErrorMessage = "O preço deverá ser entre 1 e 999.")]
+        public decimal preco { get; set; }
+
+        [Required(ErrorMessage = "O campo peso é obrigatório")]
+        public decimal peso { get; set; }
+
+        [Required(ErrorMessage = "O campo altura é obrigatório")]
+        public decimal altura { get; set; }
+
+        [Required(ErrorMessage = "O campo largura é obrigatório")]
+        public decimal largura { get; set; }
+
+        [Required(ErrorMessage = "O campo comprimento é obrigatório")]
+        public decimal comprimento { get; set; }
+
+        [Required(ErrorMessage = "O campo diametro é obrigatório")]
+        public decimal diametro { get; set; }
+
+        [StringLength(80, ErrorMessage = "O tamanho máximo da url é 80 caracteres")]
+        public string Url { get; set; }
+    }
+}
